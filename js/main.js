6af3152e6a82107b54a5ae807c13d083eab61a4e
Jake Greer <jakegreer11@gmail.com>
2019-11-27 16:50:06 -0800

    added email config:

--- js/main.js
+++ js/main.js
@@ -242,52 +242,56 @@
 	};
 	contentWayPoint();
 
-	const form = document.getElementById('contactForm');
+	const form = $('#contactForm');
 	const url = 'https://{id}.execute-api.{region}.amazonaws.com/{stage}/email/send';
-	const submit = document.getElementById('submit');
+	const results = $('#results');
+	const submit = $('#submit');
 	
 	function post(url, body, callback) {
-	  var req = new XMLHttpRequest();
-	  req.open("POST", url, true);
-	  req.setRequestHeader("Content-Type", "application/json");
-	  req.addEventListener("load", function () {
-		if (req.status < 400) {
-		  callback(null, JSON.parse(req.responseText));
-		} else {
-		  callback(new Error("Request failed: " + req.statusText));
-		}
-	  });
-	  req.send(JSON.stringify(body));
+		var req = new XMLHttpRequest();
+		req.open("POST", url, true);
+		req.setRequestHeader("Content-Type", "application/json");
+		req.addEventListener("load", function () {
+			if (req.status < 400) {
+				callback(null, JSON.parse(req.responseText));
+			} else {
+				callback(new Error("Request failed: " + req.statusText));
+			}
+		});
+		req.send(JSON.stringify(body));
 	}
+
 	function success () {
-	  toast.innerHTML = 'Thanks for sending me a message! I\'ll get in touch with you ASAP. :)'
-	  submit.disabled = false
-	  submit.blur()
-	  form.name.focus()
-	  form.name.value = ''
-	  form.email.value = ''
-	  form.content.value = ''
+		results.html('Thanks for sending me a message! I\'ll get in touch with you ASAP.');
+		submit.disabled = false;
+		submit.blur();
+		form.name.focus();
+		form.name.value = '';
+		form.email.value = '';
+		form.message.value = '';
 	}
 	function error (err) {
-	  toast.innerHTML = 'There was an error with sending your message, hold up until I fix it. Thanks for waiting.'
-	  submit.disabled = false
-	  console.log(err)
+		results.html('There was an error with sending your message, hold up until I fix it. Thanks for waiting.');
+		submit.disabled = false;
+		console.log(err);
 	}
 	
 	form.addEventListener('submit', function (e) {
-	  e.preventDefault()
-	  toast.innerHTML = 'Sending'
-	  submit.disabled = true
-	
-	  const payload = {
-		name: form.name.value,
-		email: form.email.value,
-		content: form.content.value
-	  }
-	  post(url, payload, function (err, res) {
-		if (err) { return error(err) }
-		success()
-	  })
+		e.preventDefault()
+		$('#emailModal').modal('show');
+
+		results.html('Sending');
+		submit.disabled = true;
+		
+		const payload = {
+			name: form.name.value,
+			email: form.email.value,
+			content: form.message.value
+		}
+		post(url, payload, function (err, res) {
+			if (err) { return error(err) }
+			success();
+		})
 	})
 
 
